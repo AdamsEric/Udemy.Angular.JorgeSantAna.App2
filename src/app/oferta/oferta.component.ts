fec4bdff6da367f7961c85943bbb1b371959c8fc
AdamsEric <eric.adsbrito@live.com>
2018-05-20 14:58:33 -0300

    Finalizada implementação básica de ofertas

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,20 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
 import { Oferta } from './../shared/oferta.model';
+import { OfertasService } from '../shared/ofertas.service';
 
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
-  styleUrls: ['./oferta.component.css']
+  styleUrls: ['./oferta.component.css'],
+  providers: [ OfertasService ]
 })
 export class OfertaComponent implements OnInit {
+  oferta: Oferta;
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private ofertasService: OfertasService
   ) { }
 
   ngOnInit() {
-    console.log(this.route.snapshot.params['id']);
+    this.ofertasService.getOfertaPorId(this.route.snapshot.params['id'])
+    .then((oferta: Oferta) => this.oferta = oferta);
   }
 
 }
